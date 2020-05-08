c274cc533c9ffe83784b159126b56c417e32123e
Ricardo Brasil <ricardohauus@gmail.com>
2020-05-08 13:37:20 -0300

    Adicionado o botão para criar novo registro

--- public/scripts.js
+++ public/scripts.js
@@ -2,10 +2,13 @@ const currentPage = location.pathname;
 const menuItems = document.querySelectorAll("header .links a")
 
 for (menuItem of menuItems) {
-  if (currentPage == menuItem.getAttribute("href")) {
+  console.log(currentPage, menuItem.getAttribute("href"));
+  if (currentPage.includes(menuItem.getAttribute("href"))) {
     menuItem.classList.add("active")
     break;
   }
+
+
 }
 
 
